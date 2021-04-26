3ccce0a38625d66ed56291c1b55be5955d8ff98e
xiyue <xiyuesaves@gmail.com>
2021-04-26 18:00:49 +0800

    重写部分逻辑

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,3 +1,4 @@
+var zoom = 1
 window.onload = function() {
     console.log(screen.availWidth, screen.availHeight);
     var canvas = document.querySelector("#canvas");
@@ -7,54 +8,53 @@ window.onload = function() {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
     }
-    // 刷新画布
-    refreshCanvas();
     // 获取笔刷位置
     getBrushPosition();
     // 设置菜单
-    brushSettingMenu();
+    // brushSettingMenu();
 };
 
 // 获取笔刷位置
 function getBrushPosition() {
+    let brushColor = "black"
+    let pathArr = []
+    const canvas = document.querySelector("#canvas");
+    const ctx = canvas.getContext('2d');
+    // 刷新画布
+    refreshCanvas();
+
     canvas.addEventListener('mousemove', (e) => { // 展示当前画笔位置
         const brushEl = document.querySelector(".brush");
         brushEl.style.transform = "translate3d(" + (e.offsetX - (brushEl.offsetWidth / 2)) + "px, " + (e.offsetY - (brushEl.offsetHeight / 2)) + "px, 0";
-        if (e.buttons === 1) { // 如果按着左键
-            draw(e);
-        };
+        if (e.buttons === 1) {
+            draw(e)
+            pathArr.push([e.offsetX, e.offsetY])
+        }
     });
-    canvas.addEventListener('mousedown', (e) => { // 展示当前画笔位置
-        if (e.buttons === 1) { // 如果按着左键
-            draw(e);
-        };
-    });
+
 
     function draw(e) {
         const brushEl = document.querySelector(".brush")
-        if (e.buttons === 1) { // 如果按着左键
-            const ctx = canvas.getContext('2d');
-            ctx.fillStyle = 'rgb(0,0,0)'; // 绘制颜色
-            ctx.beginPath(); // 开始绘制路径
-            ctx.arc(e.offsetX, e.offsetY, (brushEl.offsetWidth / 2), 0, 2 * Math.PI); // 绘制圆
-            ctx.fill(); // 填充路径
-        }
+        ctx.fillStyle = brushColor; // 绘制颜色
+        ctx.beginPath(); // 开始绘制路径
+        ctx.arc(e.offsetX, e.offsetY, (brushEl.offsetWidth / 2), 0, 2 * Math.PI); // 绘制圆
+        ctx.fill(); // 填充路径
+    }
+
+    // 根据屏幕刷新率来刷新canvas
+    function refreshCanvas() {
+        requestAnimationFrame(
+            function() {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                for (let i = 0; i < pathArr.length; i++) {
+                    const brushEl = document.querySelector(".brush")
+                    ctx.fillStyle = brushColor; // 绘制颜色
+                    ctx.beginPath(); // 开始绘制路径
+                    ctx.arc(pathArr[i][0] * zoom, pathArr[i][1] * zoom, ((brushEl.offsetWidth / 2) * zoom), 0, 2 * Math.PI); // 绘制圆
+                    ctx.fill(); // 填充路径
+                }
+                refreshCanvas();
+            }
+        );
     }
 };
-
-// 设置菜单
-function brushSettingMenu() {
-
-}
-
-// 根据屏幕刷新率来刷新canvas
-function refreshCanvas() {
-    requestAnimationFrame(
-        function() {
-            const canvas = document.querySelector("#canvas");
-            const ctx = canvas.getContext('2d');
-
-            refreshCanvas();
-        }
-    );
-}
