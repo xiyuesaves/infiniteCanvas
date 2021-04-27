9e50fc05803c455530fa830e7b6e563fdcc5a25a
xiyue <xiyuesaves@gmail.com>
2021-04-27 13:37:37 +0800

    重写js逻辑

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,4 +1,4 @@
-var zoom = 1000,
+var zoom = 100000,
     dZoom = 0
 
 function z(num) {
@@ -26,15 +26,16 @@ function initCanvas() {
     let brushColor = "black";
     let pathArr = [];
     let ctxOrigin = [0, 0];
-    let toTop = 0, toLeft = 0
+    let toTop = 0,
+        toLeft = 0;
+    let canvasData;
     const canvas = document.querySelector("#canvas");
     const brushEl = document.querySelector(".brush");
     const ctx = canvas.getContext('2d');
-
     window.onresize = function() {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        fullCanvas()
+        fullCanvas();
     }
 
     // 刷新画布
@@ -50,7 +51,8 @@ function initCanvas() {
         if (e.buttons === 2) {
             document.body.className = "move";
             ctxOrigin = [ctxOrigin[0] + e.movementX, ctxOrigin[1] + e.movementY]
-            fullCanvas()
+            moveCanvas()
+            // fullCanvas()
         } else {
             document.body.className = "";
         };
@@ -69,6 +71,7 @@ function initCanvas() {
         // 右键按下则记录按下初始位置
         if (e.buttons === 2) {
             document.body.className = "move";
+            // fullCanvas()
         } else {
             document.body.className = "";
         };
@@ -82,45 +85,54 @@ function initCanvas() {
     })
 
     // 画布缩放
-
-
+    canvas.addEventListener('wheel', (e) => {
+        zoom = zoom - (e.deltaY)
+    })
 
     // 绘制方法
     function draw(e) {
         ctx.fillStyle = brushColor; // 绘制颜色
         ctx.beginPath(); // 开始绘制路径
-        ctx.arc(e.offsetX , e.offsetY , (brushEl.offsetWidth / 2), 0, 2 * Math.PI); // 绘制圆
+        ctx.arc(e.offsetX, e.offsetY, (brushEl.offsetWidth / 2), 0, 2 * Math.PI); // 绘制圆
         ctx.fill(); // 填充路径
     };
 
     // 重绘画布[高资源占用]
     function fullCanvas() {
+        console.log("重绘")
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         for (let i = 0; i < pathArr.length; i++) {
             const brushWidth = brushEl.offsetWidth / 2;
             ctx.fillStyle = brushColor; // 绘制颜色
             ctx.beginPath(); // 开始绘制路径
-            ctx.arc((pathArr[i][0]+ctxOrigin[0]) / pathArr[i][2] * dZoom, (pathArr[i][1]+ctxOrigin[1]) / pathArr[i][2] * dZoom, (brushWidth / pathArr[i][2] * dZoom), 0, 2 * Math.PI); // 绘制圆
+            ctx.arc((pathArr[i][0] + ctxOrigin[0]) / pathArr[i][2] * dZoom, (pathArr[i][1] + ctxOrigin[1]) / pathArr[i][2] * dZoom, (brushWidth / pathArr[i][2] * dZoom), 0, 2 * Math.PI); // 绘制圆
             ctx.fill(); // 填充路径
         };
+        canvasData = ctx.getImageData(0, 0, canvas.width, canvas.height)
     };
 
     // 移动画布[低资源占用]
     function moveCanvas() {
-        
+        // newCanvasData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+        // if (canvasData !== newCanvasData) {
+        //     canvasData = newCanvasData
+        // }
+        ctx.translate(ctxOrigin[0], ctxOrigin[1])
+        fullCanvas()
+        // ctx.putImageData(canvasData, 0,0)
     };
 
     // 根据屏幕刷新率来刷新canvas
     function refreshCanvas() {
-        requestAnimationFrame(
-            function() {
-                // 如果改变缩放值则刷新画布
-                if (zoom !== dZoom) {
-                    dZoom = zoom;
-                    fullCanvas();
-                }
-                refreshCanvas();
-            }
-        );
+        // requestAnimationFrame(
+        //     function() {
+        //         // 如果改变缩放值则刷新画布
+        //         if (zoom !== dZoom) {
+        //             dZoom = zoom;
+        //             fullCanvas();
+        //         }
+        //         refreshCanvas();
+        //     }
+        // );
     };
 };
