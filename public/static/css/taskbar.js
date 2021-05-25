5559868657a70b7f47bd6ce29b55e5416b57ebe6
xiyue <xiyuesaves@gmail.com>
2021-05-25 18:00:48 +0800

    优化部分逻辑

--- public/static/css/taskbar.js
+++ public/static/css/taskbar.js
@@ -3,9 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const menuBtn = document.querySelector(".menu-btn");
     const taskList = document.querySelector(".task-list");
 
-    // 全局鼠标按下事件
+    // 开始菜单
     document.addEventListener("mousedown", function(e) {
-        // 开始菜单失焦
         let clickMenu = false
         for (let i = 0; i < e.path.length; i++) {
             if (e.path[i].className && e.path[i].className.includes("menu-btn")) {
@@ -16,23 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
         if (!clickMenu) {
             menuBtn.className = "menu-btn"
         }
-
-
-        // 任务栏图标失焦
-        let clickProgram = false
-        for (let i = 0; i < e.path.length; i++) {
-            if (e.path[i].className && e.path[i].className.includes("program")) {
-                clickProgram = true
-                return
-            }
-        }
-        if (!clickProgram) {
-            if (document.querySelector(".program.act")) {
-                document.querySelector(".program.act").className = "program"
-            }
-        }
     })
-    // 开始菜单点击事件
     menuBtn.addEventListener("click", function(e) {
         if (!menuBtn.className.includes("act")) {
             menuBtn.className = "menu-btn act"
@@ -50,10 +33,7 @@ document.addEventListener("DOMContentLoaded", function() {
             }
             e.path[0].className = "program act"
         } else {
-            if (document.querySelector(".program.act")) {
-                document.querySelector(".program.act").className = "program"
-            }
+            document.querySelector(".program.act").className = "program"
         }
     })
-    
 })
